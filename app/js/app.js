246d4d64580cec175d7b3068dab760b4ae7325cc
Alex <arboerstler@gmail.com>
2014-03-20 11:42:09 -0400

    all the things in one commit because Im a bad person

--- app/js/app.js
+++ app/js/app.js
@@ -21,6 +21,17 @@ var app = (function() {
 
 	//foundation init
 	$(document).foundation();
+	$(".subnav.sidebar").foundation("magellan", {
+		// specify the class used for active sections
+		active_class: 'active',
+		// how many pixels until the magellan bar sticks, 0 = auto
+		threshold: 0,
+		// pixels from the top of destination for it to be considered active
+		destination_threshold: 0,
+		// calculation throttling to increase framerate
+		throttle_delay: 50
+	});
+	console.log('lols');
 
 	app.publicFunction();
 	app.userAgentInit();
